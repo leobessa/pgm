37ba0d33c562f31b72696c57dc23246afbb84b37
leobessa <leobessa@gmail.com>
2015-06-16 18:40:37 -0300

    dilatacao

--- pgm_spec.rb
+++ pgm_spec.rb
@@ -24,6 +24,16 @@ a - ajuda
 s - sair"""
   end
 
+  let(:entrada_content) do
+"""9 4 5 0 8
+1 3 10 3 2
+1 7 5 7 9
+1 6 3 15 2
+2 0 3 8 10
+1 0 0 1 16
+9 12 7 1 3"""
+  end
+
   it 'should display banner' do
     stdin_data = %w(sample_entrada s)
     expected_output =
@@ -54,13 +64,7 @@ Digite um comando:"""
   end
 
   it 'should display matrix' do
-    input = """9 4 5 0 8
-1 3 10 3 2
-1 7 5 7 9
-1 6 3 15 2
-2 0 3 8 10
-1 0 0 1 16
-9 12 7 1 3"""
+    input = entrada_content
     stdin_data = %w(sample_entrada t s)
     File.open("#{stdin_data[0]}.pgm",'w') do |file|
       file.puts input
@@ -223,9 +227,7 @@ Digite um comando:"""
 4 3 7 6 0 0 12
 9 1 1 1 2 1 9"
     stdin_data = %w(sample_entrada e 3 t s)
-    File.open("#{stdin_data[0]}.pgm",'w') do |file|
-      file.puts input
-    end
+    File.open("#{stdin_data[0]}.pgm",'w') { |file| file.puts input }
     expected_output =
     """Digite o nome do arquivo de entrada:
 Arquivo #{stdin_data[0]}.pgm carregado com sucesso.
@@ -238,6 +240,29 @@ Digite um comando:
 0 0 3 0 0 0 0
 1 1 1 0 0 0 0
 1 1 1 0 0 0 0
+Digite um comando:"""
+    output, status = Open3.capture2e("./pgm", stdin_data: stdin_data.join("\n") + "\n")
+    expect(output).to eq(expected_output)
+  end
+  
+  it 'should display dilatacao matrix' do
+    input = entrada_content
+    stdin_data = %w(sample_entrada d 3 t s)
+    File.open("#{stdin_data[0]}.pgm",'w') { |file| file.puts input }
+    expected_output =
+    """Digite o nome do arquivo de entrada:
+Arquivo #{stdin_data[0]}.pgm carregado com sucesso.
+#{banner}
+Digite um comando:
+Digite o tamanho da janela:
+Digite um comando:
+9 10 10 10 8
+9 10 10 10 9
+7 10 15 15 15
+7 7 15 15 15
+6 6 15 16 16
+12 12 12 16 16
+12 12 12 16 16
 Digite um comando:"""
     output, status = Open3.capture2e("./pgm", stdin_data: stdin_data.join("\n") + "\n")
     expect(output).to eq(expected_output)
